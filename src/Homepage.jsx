11fd2ca9d8b6b92333b74615dbddc481b7ef18a6
alekhya <pothuhlt3014@gmail.com>
2021-05-07 09:52:05 +0530

    hemanth pothu

---
+++ src/Homepage.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+function Homepage() {
+    return (
+        <div>
+            <div className="container2">
+                <div className="container">
+                    <div className="box" >
+                        <h3>hemanth pothu</h3>
+                    </div>
+                    <div className="box" >
+                        <h3>hemanth pothu</h3>
+                    </div>
+                    <div className="box">
+                        <h3>hemanth pothu</h3>
+                    </div>
+                    <div className="box" >
+                        <h3>hemanth pothu</h3>
+                    </div>
+                </div>
+                <div className="container1">
+                    <h3>hemanth pothu</h3>
+                </div>
+            </div>
+            <div className="container3">
+                <div className="cont-1">
+                    <div className="driver-1">
+                        <h3>Lorem ipsum dolor sit.</h3>
+                    </div>
+                    <div className="hosp-1">
+                        <div className="name-1">
+                            hemanth pothu
+                </div>
+                        <div className="name-2">
+                            <h3>hemanth</h3>
+                            <h3>ramesh hospital</h3>
+                            <h3>vijayawada</h3>
+                        </div>
+                    </div>
+                </div>
+                <div className="cont-2">
+                    <div className="hosp-1">
+                        <div className="name-1">
+                            hemanth pothu
+                </div>
+                        <div className="name-2">
+                            <h3>hemanth</h3>
+                            <h3>ramesh hospital</h3>
+                            <h3>vijayawada</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+export default Homepage;
